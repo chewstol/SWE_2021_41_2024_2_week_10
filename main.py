0617d0d89263e9567d85b246f5d4220ae0c55739
chewstol <mj9962@g.skku.edu>
2024-11-06 19:30:31 +0900

    even_list

--- main.py
+++ main.py
@@ -2,15 +2,11 @@ from typing imort List
 
 # Skeleton code for even_list
 def even_list(int_list: List[int]) -> List[int]:
-    """
-    Determines if a number is even and return an even list.
-
-    Args:
-        int_list: A list of integer.
-
-    Returns:
-        A list of even integers.
-    """
+    evens = []
+    for num in int_list:
+        if num%2 == 0:
+            evens.append(num)
+    return evens
     # TODO: Implement even_list
     pass
 
